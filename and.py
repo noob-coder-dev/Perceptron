7d01d461c4e3fdf2685bde01a3e2e100a587a31d
Manojit Roy <66888595+noob-coder-dev@users.noreply.github.com>
2021-10-13 13:28:55 +0530

    Modular Code

--- and.py
+++ and.py
@@ -3,25 +3,33 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
-AND = {
-    'x1':[1,1,0,0],
-    'x2':[1,0,1,0],
-    'y':[1,0,0,0]
-}
+def main(data, eta, epochs, modelFileName, plotFileName):
 
-df = pd.DataFrame(AND)
+    df = pd.DataFrame(data)
 
-print(df)
+    print(df)
 
-X, y = prepare_data(df)
+    X, y = prepare_data(df)
 
-eta = 0.3 # 0 and 1
-epochs = 10
 
-model = Perceptron(eta=eta, epochs=epochs)
-model.fit(X, y)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-_ = model.total_loss()
+    _ = model.total_loss()
 
-save_model(model, filename="and.model")
-save_plot(df, filename="and.png", model=model)
+    save_model(model, filename=modelFileName)
+    save_plot(df, filename=plotFileName, model=model)
+    
+
+if __name__ == "__main__":
+    AND = {
+        'x1':[1,1,0,0],
+        'x2':[1,0,1,0],
+        'y':[1,0,0,0]
+    }
+
+    eta = 0.3 # 0 and 1
+    epochs = 10
+
+    main(data = AND, eta=eta, epochs=epochs, modelFileName="and.model", plotFileName="and.png")
+
